775dbb4f9d68850e4b23e3506d947bd715443a96
dmtihor <dmtihor@gmail.com>
2015-12-13 16:01:48 +0200

    + String + Composition

--- src/Car.java
+++ src/Car.java
@@ -11,29 +11,24 @@ public class Car implements Serializable {
     String color;
     transient Engine e;
 
-    public Car(int speed, String color) {
-        this.speed = speed;
-        this.color = color;
-    }
 
-    public Car(int speed, Engine e, String color) {
+    public Car(int speed, String color, Engine e) {
         this.speed = speed;
+
+        this.color = color;
         this.e = e;
-        this.color = color;
-    }
-
-    public Car(int speed) {
-        this.speed = speed;
     }
 
     private void writeObject(ObjectOutputStream os) throws IOException {
         os.defaultWriteObject();
         os.writeInt(e.power);
         os.writeObject(e.producer);
+        os.writeInt(e.d.fuelCount);
     }
+
     private void readObject(ObjectInputStream is) throws IOException, ClassNotFoundException {
         is.defaultReadObject();
-        e = new Engine(is.readInt(),(String)is.readObject());
+        e = new Engine(is.readInt(), (String) is.readObject(), new Diesel(is.readInt()));
     }
 
 }
